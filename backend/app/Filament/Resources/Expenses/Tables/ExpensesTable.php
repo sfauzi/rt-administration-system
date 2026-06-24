6ccaebdcfe481fdf1c0c0d3a248430461d31a132
sfauzi <hello@sfauzi.dev>
2026-06-24 15:05:25 +0700

    feat: :sparkles: create expense resource panel

---
+++ backend/app/Filament/Resources/Expenses/Tables/ExpensesTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Expenses\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ExpensesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->searchable(),
+                BadgeColumn::make('category'),
+                TextColumn::make('amount')->money('IDR', locale: 'id'),
+                TextColumn::make('expense_date')->date()->sortable(),
+                TextColumn::make('expense_month'),
+                IconColumn::make('is_recurring')->boolean(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
