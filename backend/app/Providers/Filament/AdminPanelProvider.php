c653d3cec723bae5b32672d36ea2167d6c238575
sfauzi <hello@sfauzi.dev>
2026-06-25 12:22:17 +0700

    fix: :bug: change base color primary

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->authGuard('web')
             // ->bootUsing(function (Panel $panel) {
