f30fbb1c04f495502a602ac0e4c8e318b32a7167
sfauzi <hello@sfauzi.dev>
2026-06-25 12:22:36 +0700

    fix: :bug: fix return redirect base route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\SsoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('filament.admin.auth.login');
 });
 
 // ── SSO Routes ────────────────────────────────────────────────────────────────
